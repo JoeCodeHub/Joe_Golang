c616575510eba26741a82d4fea9976109ec896a6
Joe <news.tina@gmail.com>
2012-04-10 14:19:46 +0800

    a

---
+++ 01_LearnExampleSRC/httpClient/httpClient.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+    "flag"
+    "fmt"
+    "io/ioutil"
+    "net/http"
+    "os"
+    "encoding/xml"
+)
+
+type struct 
+
+func xmlFile(path string) {
+
+    fmt.Printf("xmlpth: [%s]\n", path)
+
+}
+
+func writefile(data []byte, fpath string) {
+
+    fout, err := os.Create(fpath)
+
+    if err != nil {
+        fmt.Printf("Create file : %v", err)
+    }
+
+    fout.Write(data)
+    fout.Close()
+}
+
+func main() {
+
+    flag.Parse()
+    s := ""
+
+    xmlPath := "/tmp/joe.xu.xmlFile.httpClient"
+
+    if flag.NArg() < 1 {
+        s = "http://blog.codingnow.com/atom.xml"
+    } else {
+        for i := 0; i < flag.NArg(); i++ {
+            s += flag.Arg(i)
+        }
+    }
+
+    fmt.Printf("Args : [%s]\n", s)
+
+    res, err := http.Get(s)
+
+    if err != nil {
+        fmt.Printf("Get : %v\nexit!\n", err)
+        os.Exit(1)
+    }
+    data, err := ioutil.ReadAll(res.Body)
+    if err != nil {
+        fmt.Printf("ReadAll : %v", err)
+    }
+    fmt.Printf("Get Success : [%s]  Save to [%s] \r\n", s, xmlPath)
+
+    writefile(data, xmlPath)
+
+    xmlFile(xmlPath)
+}
